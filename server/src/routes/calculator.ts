d1adda372b8d24dd9d7630c85c68af9daabfb163
Eric Connelly <eric.connelly08@gmail.com>
2025-04-11 10:59:53 -0400

    feat: simple POC roundtrip for submit form to backend

--- server/src/routes/calculator.ts
+++ server/src/routes/calculator.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/calculate-success-rate', (req: Request, res: Response) => {
     const { isUsingOwnEggs, hasPrevIVF, reasonForIVF } = req.body;
+    console.log('Handling request with body:', req.body);
     let successRate = calculate(reasonForIVF);
     res.status(200).json({
         successRate: successRate,
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
+import {Button, FieldError, Form, Input, Label, TextField} from 'react-aria-components';
 import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [isLoading, setIsLoading] = useState(true);
   const [backendMessage, setBackendMessage] = useState('');
 
@@ -23,6 +23,20 @@ function App() {
     }, 1000);
   }, []);
 
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Handle form submission
+    let data = Object.fromEntries(new FormData(event.currentTarget));
+    axios.post('/api/calculate-success-rate', JSON.stringify(data))
+      .then(response => {
+        console.log('Success rate:', response.data.successRate);
+      })
+      .catch(error => {
+        console.error('Error submitting form:', error);
+      });
+    console.log('Form submitted', event);
+  }
+
   return (
     <>
       <h1>New Demo App with Vite + React!</h1>
@@ -34,13 +48,15 @@ function App() {
           <p>{backendMessage}</p>
         )}
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="form-container">
+      <Form onSubmit={onSubmit}>
+        <TextField name="email" type="email" isRequired>
+          <Label>Email</Label>
+          <Input />
+          <FieldError />
+        </TextField>
+        <Button type="submit">Submit</Button>
+      </Form>
       </div>
     </>
   )
