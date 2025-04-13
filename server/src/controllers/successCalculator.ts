3a64888d6251b115d06e58526c227faf641c22fd
Eric Connelly <eric.connelly08@gmail.com>
2025-04-13 14:16:50 -0400

    feat: add improved types and method for getting correct formula on backend

--- server/src/controllers/successCalculator.ts
+++ server/src/controllers/successCalculator.ts
@@ -2,23 +2,41 @@ import fs from 'fs';
 import path from 'path';
 import csv from 'csv-parser';
 
-async function parseCsv(filePath: string): Promise<Record<string, string>[]> {
+async function parseCsv(filePath: string): Promise<IVFSuccessFormulaRow[]> {
   return new Promise((resolve, reject) => {
-    const results: Record<string, string>[] = [];
+    const results: IVFSuccessFormulaRow[] = [];
 
     fs.createReadStream(filePath)
       .pipe(csv())
-      .on('data', (data) => results.push(data))
-      .on('end', () => resolve(results))
+      .on('data', (data) => {
+        // Transform each row to parse values
+        const transformedRow = Object.fromEntries(
+          Object.entries(data).map(([key, value]) => {
+            if (value === 'TRUE') return [key, true]; // Convert "TRUE" to true
+            if (value === 'FALSE' || value === 'N/A') return [key, false]; // Convert "FALSE" to false
+            if (!isNaN(Number(value))) return [key, Number(value)]; // Convert numeric strings to numbers
+            return [key, value]; // Keep other values as strings
+          })
+        ) as IVFSuccessFormulaRow;
+
+        results.push(transformedRow);
+      })
+      
+      .on('end', () => resolve(results as IVFSuccessFormulaRow[]))
       .on('error', (error) => reject(error));
   });
 }
 
-type FormulaSelectionParams = {
-  isUsingOwnEggs: boolean;
-  hasPrevIVF: boolean;
-  isInfertilityReasonKnown: boolean;
-};
+type FormulaSelectionParams =
+  | {
+      isUsingOwnEggs: true;
+      hasPrevIVF: boolean; // Required when isUsingOwnEggs is true
+      isInfertilityReasonKnown: boolean;
+    }
+  | {
+      isUsingOwnEggs: false;
+      isInfertilityReasonKnown: boolean;
+    };
 
 type FormulaCalculationParams = {
   age: number;
@@ -29,13 +47,94 @@ type FormulaCalculationParams = {
 };
 
 type InfertilityReasonParams = {
+  tubal_factor: boolean;
+  male_factor_infertility: boolean;
+  endometriosis: boolean;
+  ovulatory_disorder: boolean;
   diminished_ovarian_reserve: boolean;
   uterine_factor: boolean;
+  other_reason: boolean;
+  unexplained_infertility: boolean;
 };
 
-export async function calculate(formulaParams: FormulaSelectionParams, reasonForIVF: string): Promise<number> {
+// create a custom type to represent integers
+type Integer = number & { __integer: true };
+
+// This type defines the structure of the data in the CSV file
+type IVFSuccessFormulaRow = {
+  // Parameters for finding the relevant formula
+  param_using_own_eggs: boolean;
+  param_attempted_ivf_previously: boolean;
+  param_is_reason_for_infertility_known: boolean;
+  cdc_formula: string;
+  // calculation params for Age & BMI score
+  formula_intercept: number;
+  formula_age_linear_coefficient: number;
+  formula_age_power_coefficient: number;
+  formula_age_power_factor: number;
+  formula_bmi_linear_coefficient: number;
+  formula_bmi_power_coefficient: number;
+  formula_bmi_power_factor: Integer;
+  // coefficients for the infertility reasons
+  formula_tubal_factor_true_value: number;
+  formula_tubal_factor_false_value: Integer;
+  formula_male_factor_infertility_true_value: number;
+  formula_male_factor_infertility_false_value: Integer;
+  formula_endometriosis_true_value: number;
+  formula_endometriosis_false_value: Integer;
+  formula_ovulatory_disorder_true_value: number;
+  formula_ovulatory_disorder_false_value: Integer;
+  formula_diminished_ovarian_reserve_true_value: number;
+  formula_diminished_ovarian_reserve_false_value: Integer;
+  formula_uterine_factor_true_value: number;
+  formula_uterine_factor_false_value: Integer;
+  formula_other_reason_true_value: number;
+  formula_other_reason_false_value: Integer;
+  formula_unexplained_infertility_true_value: number;
+  formula_unexplained_infertility_false_value: Integer;
+  // coefficients for the number of prior pregnancies
+  formula_prior_pregnancies_0_value: Integer;
+  formula_prior_pregnancies_1_value: number;
+  'formula_prior_pregnancies_2+_value': number;
+  // coefficients for the number of prior births
+  formula_prior_live_births_0_value: Integer;
+  formula_prior_live_births_1_value: number;
+  'formula_prior_live_births_2+_value': number;
+};
+type IVFSuccessFormulaRowKeys = keyof IVFSuccessFormulaRow;
+
+
+const formulaPropToHeaderMap: Record<string, IVFSuccessFormulaRowKeys> = {
+  isUsingOwnEggs: 'param_using_own_eggs',
+  hasPrevIVF: 'param_attempted_ivf_previously',
+  isInfertilityReasonKnown: 'param_is_reason_for_infertility_known',
+};
+
+export async function getFormula(formulaParams: FormulaSelectionParams): Promise<IVFSuccessFormulaRow> {
   const filePath = path.join(path.dirname(__dirname), 'data', 'ivf_success_formulas.csv');
-  console.log('File path:', filePath);
+  let data: IVFSuccessFormulaRow[] = [];
+
+  try {
+    data = await parseCsv(filePath);
+    console.log('Parsed CSV Data:', data);
+  } catch (error) {
+    console.error('Error parsing CSV:', error);
+  }
+
+  // Filter the Forumla rows to match the selection criteria in formulaParams
+  const formula = data.find((row: IVFSuccessFormulaRow) => {
+    return row.param_using_own_eggs === formulaParams.isUsingOwnEggs &&
+           (!formulaParams.isUsingOwnEggs || row.param_attempted_ivf_previously === formulaParams.hasPrevIVF) &&
+           row.param_is_reason_for_infertility_known === formulaParams.isInfertilityReasonKnown;
+  });
+  if (!formula) {
+    throw new Error('No matching formula found');
+  }
+  return formula;
+}
+
+export async function calculate(reasonForIVF: string): Promise<number> {
+  const filePath = path.join(path.dirname(__dirname), 'data', 'ivf_success_formulas.csv');
   try {
     const data = await parseCsv(filePath);
     console.log('Parsed CSV Data:', data);
--- server/src/routes/calculator.ts
+++ server/src/routes/calculator.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from 'express';
-import { calculate } from '../controllers/successCalculator';
+import { getFormula, calculate } from '../controllers/successCalculator';
 
 const router = express.Router();
 
 router.post('/calculate-success-rate', async (req: Request, res: Response) => {
     const { isUsingOwnEggs, hasPrevIVF, reasonForIVF } = req.body;
     console.log('Handling request with body:', req.body);
+
+    const formula = await getFormula(req.body);
+    console.log('Formula:', formula);
     let successRate = await calculate(reasonForIVF);
     res.status(200).json({
         successRate: successRate,
